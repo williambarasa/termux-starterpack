81786cb892b5a2ac9397d55790304c650f7b9532
williambarasa <100489181+williambarasa@users.noreply.github.com>
2023-09-23 14:15:57 -0500

    Update Install.sh
    added and removed packages

--- Install.sh
+++ Install.sh
@@ -38,7 +38,10 @@ echo -e "${YELLOW}Installing essential packages...${NC}"
 essential_packages=(
   git python python2 python3 curl wget tar zip unzip openssl openssh nano vim build-essential
   clang gdb valgrind
-  neofetch
+  neofetch fzf zsh tigervnc fish
+  tcsh emacs neovim golang ecj openjdk-17 
+  lua54 lua53 lua52 php ruby rust swift 
+  proot proot-distro  
 )
 for package in "${essential_packages[@]}"; do
   echo -ne "${BLUE}Installing ${package}${NC}"
